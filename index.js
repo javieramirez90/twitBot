cb6fab354865272a5d16490b7528503869c485df
javieramirez90 <javier.ramirez@aiesec.net>
2018-10-26 21:10:27 -0500

    Error: Twit config must include  when using user auth

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
+require('dotenv').config()
+
 var Twit = require('twit');
+
+
 
 var bot = new Twit({
   consumer_key: process.env.LEARNINGBOT_CONSUMER_KEY,
   consumer_secret: process.env.LEARNINGBOT_CONSUMER_SECRET,
   access_token: process.env.LEARNINGBOT_ACCESS_TOKEN,
   access_token_secret: process.env.LEARNINGBOT_ACCESS_TOKEN_SECRET,
-  timeout_ms: 60 * 1000//for HTTP request
-})
+  timeout_ms: 60*1000
+});
+
 
 //TO TWIT WITH MY ACCOUNT
 // bot.post('statuses/update', {status: "Hello world, using twit.js"}, function(err,data,response){
@@ -17,7 +22,7 @@ var bot = new Twit({
 //   }
 // })
 
-bot.get('followers/ids', {screen_name: 'bots_with_han'}, function(err, data, response){
+bot.get('followers/list', {screen_name: 'bots_with_han'}, function(err, data, response){
   if(err){
     console.log(err);
   } else {
