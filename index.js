154d94ae1b48ca1c3040cc30be7e88f8b66f193a
javieramirez90 <javier.ramirez@aiesec.net>
2018-10-26 21:41:55 -0500

    Solved. It was the dotenv module config

--- index.js
+++ index.js
@@ -2,30 +2,29 @@ require('dotenv').config()
 
 var Twit = require('twit');
 
-
-
 var bot = new Twit({
-  consumer_key: process.env.LEARNINGBOT_CONSUMER_KEY,
-  consumer_secret: process.env.LEARNINGBOT_CONSUMER_SECRET,
-  access_token: process.env.LEARNINGBOT_ACCESS_TOKEN,
-  access_token_secret: process.env.LEARNINGBOT_ACCESS_TOKEN_SECRET,
-  timeout_ms: 60*1000
+    consumer_key: process.env.LEARNINGBOT_CONSUMER_KEY,
+    consumer_secret: process.env.LEARNINGBOT_CONSUMER_SECRET,
+    access_token: process.env.LEARNINGBOT_ACCESS_TOKEN,
+    access_token_secret: process.env.LEARNINGBOT_ACCESS_TOKEN_SECRET,
+    timeout_ms: 60*1000
 });
 
 
-//TO TWIT WITH MY ACCOUNT
-// bot.post('statuses/update', {status: "Hello world, using twit.js"}, function(err,data,response){
-//   if(err) {
-//      console.log(err);
+// bot.post('statuses/update', {status: 'hello world!'}, function(err, data, response){
+//   if (err){
+//       console.log(err);
 //   } else {
-//     console.log(data.text + 'was tweeted.');
+//       console.log(data.text + ' was tweeted.');
 //   }
-// })
+// });
 
 bot.get('followers/list', {screen_name: 'bots_with_han'}, function(err, data, response){
   if(err){
-    console.log(err);
+    console.log(err)
   } else {
-    console.log(data);
+    data.users.forEach(function(user){
+      console.log(user.screen_name);
+    });
   }
 });
